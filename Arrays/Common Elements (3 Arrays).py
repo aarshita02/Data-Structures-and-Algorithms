93b184b79302acfbeaa90eae3b012b398a1b4cb2
Aarshita Acharya <aarshita08@gmail.com>
2021-07-13 11:55:56 +0530

    Changed the Time Complexity of the code
    Added an alternate solution to this code, which can run in the expected time frame.

--- Arrays/Common Elements (3 Arrays).py
+++ Arrays/Common Elements (3 Arrays).py
@@ -49,3 +49,52 @@ Output: 20 80
 Explanation: 20 and 80 are the only
 common elements in A, B and C.
 """
+
+"""
+Another solution : Accepted by geeks considering time constraints
+"""
+
+class Solution:
+    def commonElements (self,A, B, C, n1, n2, n3):
+        i, j, k = 0, 0, 0
+        result = []
+        res1 =[]
+        while (i < n1 and j < n2 and k< n3):
+            if (A[i] == B[j] and B[j] == C[k]):
+                result.append(A[i])
+                i += 1
+                j += 1
+                k += 1
+            
+            elif A[i] < B[j]:
+                i += 1
+             
+            elif B[j] < C[k]:
+                j += 1
+           
+            else:
+                k += 1
+        res1 = []
+        for l in result:
+            if l not in res1:
+                res1.append(l)
+        return res1
+       
+
+t = int (input ())
+for tc in range (t):
+    n1, n2, n3 = list(map(int,input().split()))
+    A = list(map(int,input().split()))
+    B = list(map(int,input().split()))
+    C = list(map(int,input().split()))
+    
+    ob = Solution()
+    
+    res = ob.commonElements (A, B, C, n1, n2, n3)
+    
+    if len (res) == 0:
+        print (-1)
+    else:
+        for i in range (len (res)):
+            print (res[i], end=" ")
+        print ()
