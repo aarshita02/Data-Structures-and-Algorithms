d2da1ced461c4fa13086346a1be279a2c3ace465
Aarshita Acharya <aarshita08@gmail.com>
2021-07-14 22:19:08 +0530

    Create Reverse String.py

---
+++ Strings/Reverse String.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1
+"""
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+"""
